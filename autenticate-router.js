fa4a9b280b5643b46e73b118f75796d245f0bf2e
Wagner dos Santos <wagner-power@hotmail.com>
2017-09-17 01:26:11 -0300

    Verificando Token (apenas Teste)

--- autenticate-router.js
+++ autenticate-router.js
@@ -13,11 +13,25 @@ router.post('/authenticate', function (req, res) {
         throw new Error('Senha ou usuário incorretos!');
     }
 
-    var token = jwt.sign({nome: 'Wagner Santos'}, config.segredo, {
+    var token = jwt.sign({ nome: 'Wagner Santos' }, config.segredo, {
         expiresIn: '2M'
     });
 
-    res.json({mensagem: 'Bem vindo', token: token}).status(200);
+    res.json({ mensagem: 'Bem vindo', token: token }).status(200);
+})
+
+router.get('/authenticate', function (req, res) {
+
+    jwt.verify(req.query.token, config.segredo, function (err, decoded) {
+
+        if (err){
+
+            throw new Error(err);
+        }        
+        
+        res.send(decoded)
+    });
+   
 })
 
 module.exports = router;
